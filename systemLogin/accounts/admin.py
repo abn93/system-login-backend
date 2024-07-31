fab970e9750b0c5f69473294bafa9fb73f44a3ee
Alessandro Nascimento <alexbrito-10@hotmail.com>
2024-07-31 17:17:04 -0400

    create user in admin django

--- systemLogin/accounts/admin.py
+++ systemLogin/accounts/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
 
-# Register your models here.
+
+# Formulário de criação de usuário
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ('username', 'email', 'is_admin')
+
+
+# Formulário de alteração de usuário
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+        fields = ('username', 'email', 'is_admin')
+
+
+# Customizando o UserAdmin para o CustomUser
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('username', 'email', 'is_admin', 'is_staff')
+    list_filter = ('is_admin', 'is_staff', 'is_superuser', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_admin', 'is_staff', 'is_active', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+            'username', 'email', 'password1', 'password2', 'is_admin', 'is_staff', 'is_active', 'is_superuser')}
+         ),
+    )
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+
+# Registrando o CustomUser no Django Admin
+admin.site.register(CustomUser, CustomUserAdmin)
